7ae616ab616e752ef37a2cbf59e7321c6ffc830f
Nikolay Bachiyski <nb@nikolay.bg>
2008-03-09 21:03:32 +0000

    Add a script for adding textdomain to i18n calls
    git-svn-id: https://i18n.svn.wordpress.org/tools/trunk@3401 2066f16a-a1ef-0310-b9f2-d44eba5528fd

---
+++ add-textdomain.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Console application, which adds textdomain argument
+ * to all i18n function calls
+ *
+ * @author 
+ * @version $Id$
+ * @package wordpress-i18n
+ */
+error_reporting(E_ALL);
+
+if (!isset($argv[1]) || !isset($argv[2])) {
+	die("Adds the string <domain> as a last argument to all i18n function calls in <file>\nand prints the modified php file on standard output.\nUsage: {$argv[0]} <file> <domain>\n");
+}
+
+$funcs = array('__', '_e', '_c', '__ngettext');
+$domain = addslashes($argv[2]);
+
+
+$source = file_get_contents($argv[1]);
+$tokens = token_get_all($source);
+
+$in_func = false;
+$args_started = false;
+$parens_balance = 0;
+
+foreach($tokens as $token) {
+	$string_success = false;
+	if (is_array($token)) {
+		list($id, $text) = $token;
+		if (T_STRING == $id && in_array($text, $funcs)) {
+			$in_func = true;
+			$parens_balance = 0;
+			$args_started = false;
+		}
+		$token = $text;
+	} elseif ('(' == $token){
+		$args_started = true;
+		++$parens_balance;
+	} elseif (')' == $token) {
+		--$parens_balance;
+		if ($in_func && 0 == $parens_balance) {
+			$in_func = false;
+			$args_started = false;
+			$token = ", '$domain')";
+		}
+	}
+	echo $token;
+}
+
+?>
