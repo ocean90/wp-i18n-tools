fb1da0e45878db4aabf518b9fd092e1202076e29
Nikolay Bachiyski <nb@nikolay.bg>
2008-05-05 07:26:21 +0000

    Transform add-textdomain to a class, so that we can include it safely
    git-svn-id: https://i18n.svn.wordpress.org/tools/trunk@4286 2066f16a-a1ef-0310-b9f2-d44eba5528fd

--- add-textdomain.php
+++ add-textdomain.php
@@ -9,84 +9,94 @@
  */
 error_reporting(E_ALL);
 
+class AddTextdomain {
 
-$inplace = false;
-$modified_contents = '';
+	var $modified_contents = '';
+	var $funcs = array('__', '_e', '_c', '__ngettext');
 
-function usage() {
-	$usage = "Usage: php add-textdomain.php [-i] <domain> <file>\n\nAdds the string <domain> as a last argument to all i18n function calls in <file>\nand prints the modified php file on standard output.\n\nOptions:\n    -i    Modifies the PHP file in place, instead of printing it to standard output.\n";
-	fwrite(STDERR, $usage);
-	exit(1);
-}
+	function usage() {
+		$usage = "Usage: php add-textdomain.php [-i] <domain> <file>\n\nAdds the string <domain> as a last argument to all i18n function calls in <file>\nand prints the modified php file on standard output.\n\nOptions:\n    -i    Modifies the PHP file in place, instead of printing it to standard output.\n";
+		fwrite(STDERR, $usage);
+		exit(1);
+	}
 
-function process_token($token_text) {
-	global $inplace;
-	global $modified_contents;
-
-	if ($inplace)
-		$modified_contents .= $token_text;
-	else
-		echo $token_text;
-}
+	function process_token($token_text, $inplace) {
+		if ($inplace)
+			$this->modified_contents .= $token_text;
+		else
+			echo $token_text;
+	}
 
 
-if (!isset($argv[1]) || !isset($argv[2])) {
-	usage();
-}
+	function process_file($domain, $source_filename, $inplace) {
 
-if ('-i' == $argv[1]) {
-	$inplace = true;
-	if (!isset($argv[3])) usage();
-	array_shift($argv);	
-}
+		$this->modified_contents = '';
 
-$funcs = array('__', '_e', '_c', '__ngettext');
-$domain = addslashes($argv[1]);
-$source_filename = $argv[2];
+		$source = file_get_contents($source_filename);
+		$tokens = token_get_all($source);
 
+		$in_func = false;
+		$args_started = false;
+		$parens_balance = 0;
+		$found_domain = false;
 
-$source = file_get_contents($source_filename);
-$tokens = token_get_all($source);
-
-$in_func = false;
-$args_started = false;
-$parens_balance = 0;
-$found_domain = false;
-
-foreach($tokens as $token) {
-	$string_success = false;
-	if (is_array($token)) {
-		list($id, $text) = $token;
-		if (T_STRING == $id && in_array($text, $funcs)) {
-			$in_func = true;
-			$parens_balance = 0;
-			$args_started = false;
-			$found_domain = false;
-		} elseif (T_CONSTANT_ENCAPSED_STRING == $id && ("'$domain'" == $text || "\"$domain\"" == $text)) {
-			if ($in_func && $args_started) {
-				$found_domain = true;
+		foreach($tokens as $token) {
+			$string_success = false;
+			if (is_array($token)) {
+				list($id, $text) = $token;
+				if (T_STRING == $id && in_array($text, $this->funcs)) {
+					$in_func = true;
+					$parens_balance = 0;
+					$args_started = false;
+					$found_domain = false;
+				} elseif (T_CONSTANT_ENCAPSED_STRING == $id && ("'$domain'" == $text || "\"$domain\"" == $text)) {
+					if ($in_func && $args_started) {
+						$found_domain = true;
+					}
+				}
+				$token = $text;
+			} elseif ('(' == $token){
+				$args_started = true;
+				++$parens_balance;
+			} elseif (')' == $token) {
+				--$parens_balance;
+				if ($in_func && 0 == $parens_balance) {
+					$token = $found_domain? ')' : ", '$domain')";
+					$in_func = false;
+					$args_started = false;
+					$found_domain = false;
+				}
 			}
+			$this->process_token($token, $inplace);
 		}
-		$token = $text;
-	} elseif ('(' == $token){
-		$args_started = true;
-		++$parens_balance;
-	} elseif (')' == $token) {
-		--$parens_balance;
-		if ($in_func && 0 == $parens_balance) {
-			$token = $found_domain? ')' : ", '$domain')";
-			$in_func = false;
-			$args_started = false;
-			$found_domain = false;
+
+		if ($inplace) {
+			$f = fopen($source_filename, 'w');
+			fwrite($f, $this->modified_contents);
+			fclose($f);
 		}
 	}
-	process_token($token);
 }
 
-if ($inplace) {
-	$f = fopen($source_filename, 'w');
-	fwrite($f, $modified_contents);
-	fclose($f);
+
+// run the CLI only if the file
+// wasn't included
+$included_files = get_included_files();
+if ($included_files[0] == __FILE__) {
+	$adddomain = new AddTextdomain;
+
+	if (!isset($argv[1]) || !isset($argv[2])) {
+		$adddomain->usage();
+	}
+
+	$inplace = false;
+	if ('-i' == $argv[1]) {
+		$inplace = true;
+		if (!isset($argv[3])) $adddomain->usage();
+		array_shift($argv);	
+	}
+
+	$adddomain->process_file(addslashes($argv[1]), $argv[2], $inplace);
 }
 
 ?>
