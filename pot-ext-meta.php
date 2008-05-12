5f4d9c8690d96a20ce74259b51f7c59f7abd2b1a
Nikolay Bachiyski <nb@nikolay.bg>
2008-05-12 14:46:54 +0000

    Make pot-ext-meta a class and do not duplicate functionality from makepot, but use it
    git-svn-id: https://i18n.svn.wordpress.org/tools/trunk@4330 2066f16a-a1ef-0310-b9f2-d44eba5528fd

--- pot-ext-meta.php
+++ pot-ext-meta.php
@@ -11,75 +11,69 @@
 error_reporting(E_ALL);
 
 require 'pomo/po.php';
+require 'makepot.php';
+
+class PotExtMeta {
+
+	var $headers = array(
+		'Plugin Name',
+		'Theme Name',
+		'Plugin URI',
+		'Theme URI',
+		'Description',
+		'Author',
+		'Author URI',
+		'Tags',
+	);
 
 
-define('HEADERS_MAX_LINES', 20);
-
-function usage() {
-	stderr("Usage: php pot-ext-meta.php EXT POT");
-	stderr("Adds metadata from a WordPress theme or plugin file EXT to POT file");
-}
-
-function stderr($msg, $nl=true) {
-	fwrite(STDERR, $msg.($nl? "\n" : ""));
-}
-
-function cli_die($msg) {
-	stderr($msg);
-	exit(1);
-}
-
-$headers = array(
-	'Plugin Name',
-	'Theme Name',
-	'Plugin URI',
-	'Theme URI',
-	'Description',
-	'Author',
-	'Author URI',
-	'Tags',
-);
-
-if (count($argv) < 2 || count($argv) > 3) {
-	usage();
-	exit(1);
-}
-
-$ext_filename = $argv[1];
-$pot_filename = (isset($argv[2]))? $argv[2] : '-';
-
-$extf = @fopen($ext_filename, 'r');
-if (false === $extf) {
-	cli_die("Couldn't open extension file: $ext_filename!");
-}
-
-$potf = '-' == $pot_filename? STDOUT : @fopen($pot_filename, 'a');
-if (false === $potf) {
-	cli_die("Couldn't open pot file: $pot_filename!");
-}
-
-$first_lines	= '';
-foreach(range(1, HEADERS_MAX_LINES) as $x) {
-	if (feof($extf)) break;
-	$line = fgets($extf);
-	if (false === $line) {
-		cli_die("Error reading line $x from $ext_filename!");
+	function usage() {
+		stderr("Usage: php pot-ext-meta.php EXT POT");
+		stderr("Adds metadata from a WordPress theme or plugin file EXT to POT file");
+		exit(1);
 	}
-	$first_lines .= $line;
-}
-foreach($headers as $header) {
-	if (preg_match('|'.$header.':(.*)$|mi', $first_lines, $matches)) {
-		$string = trim($matches[1]);
-		if (empty($string)) continue;
-		$args = array(
-			'singular' => $string,
-			'extracted_comments' => $header.' of an extension',
-		);
-		$entry = new Translation_Entry($args);
-		fwrite($potf, "\n".PO::export_entry($entry)."\n");
+
+	function get_pot($ext_filename) {
+		$source = MakePOT::get_first_lines($ext_filename); 
+		$pot = '';
+		foreach($this->headers as $header) {
+			$string = MakePOT::get_addon_header($header, $source);
+			if (!$string) continue;
+			$args = array(
+				'singular' => $string,
+				'extracted_comments' => $header.' of an extension',
+			);
+			$entry = new Translation_Entry($args);
+			$pot .= "\n".PO::export_entry($entry)."\n";
+		}
+		return $pot;
+	}
+
+	function append($ext_filename, $pot_filename) {
+		$pot = $this->get_pot($ext_filename);
+		$potf = '-' == $pot_filename? STDOUT : fopen($pot_filename, 'a');
+		if (!$potf) return false;
+		fwrite($potf, $pot);
+		if ('-' != $pot_filename) fclose($potf);
+
 	}
 }
-fclose($extf);
-if ('-' != $pot_filename) fclose($potf);
+
+
+$included_files = get_included_files();
+if ($included_files[0] == __FILE__) {
+	$potextmeta = new PotExtMeta;
+	if (!isset($argv[1])) {
+		$potextmeta->usage();
+	}
+	$potextmeta->append($argv[1], isset($argv[2])? $argv[2] : '-');
+}
+
+
+
+
+
+
+
 
 ?>
