62556b4f27891e08a87b68bf7bdc604d0888f1a1
Nikolay Bachiyski <nb@nikolay.bg>
2008-05-14 11:54:57 +0000

    Fix warning
    git-svn-id: https://i18n.svn.wordpress.org/tools/trunk@4351 2066f16a-a1ef-0310-b9f2-d44eba5528fd

--- makepot.php
+++ makepot.php
@@ -149,7 +149,7 @@ class MakePOT {
 $included_files = get_included_files();
 if ($included_files[0] == __FILE__) {
 	$makepot = new MakePOT;
-	if ((3 == count($argv) || 4 == count($argv)) && in_array($method = str_replace('-', '_', $argv[1]), get_class_methods(&$makepot))) {
+	if ((3 == count($argv) || 4 == count($argv)) && in_array($method = str_replace('-', '_', $argv[1]), get_class_methods($makepot))) {
 		$res = call_user_func(array(&$makepot, $method), realpath($argv[2]), isset($argv[3])? $argv[3] : null);
 		if (0 != $res) {
 			fwrite(STDERR, "xgettext returned exit code $res!\n");
