0b194639f6e0993dbf09c99f5ccaafaa108e6bbf
Nikolay Bachiyski <nb@nikolay.bg>
2009-02-16 14:40:28 +0000

    _x's first argument is the translation and the second is the context
    git-svn-id: https://i18n.svn.wordpress.org/tools/trunk@7511 2066f16a-a1ef-0310-b9f2-d44eba5528fd

--- makepot.php
+++ makepot.php
@@ -18,7 +18,7 @@ class MakePOT {
 		'__', '_e', '_c',
 		'__ngettext:1,2', '_n:1,2', '_nc:1,2',
 		'__ngettext_noop:1,2', '_n_noop:1,2',
-		'_x:1c', '_nx:1,2,4c',
+		'_x:1,2c', '_nx:1,2,4c',
 	);
 
 	var $xgettext_options = array(
