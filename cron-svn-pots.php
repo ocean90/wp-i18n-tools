c4694b0ec3c446c3fb38863adb17dc141d182d13
Nikolay Bachiyski <nb@nikolay.bg>
2010-03-05 13:42:17 +0000

    Style update
    git-svn-id: https://i18n.svn.wordpress.org/tools/trunk@11943 2066f16a-a1ef-0310-b9f2-d44eba5528fd

--- cron-svn-pots.php
+++ cron-svn-pots.php
@@ -1,8 +1,8 @@
 <?php
 require_once 'makepot.php';
 
-$options = getopt("c:p:m:n:s");
-if (empty($options)) {
+$options = getopt( 'c:p:m:n:s' );
+if ( empty( $options) ) {
 ?>
 	-c	Application svn checkout
 	-p	POT svn checkout
@@ -12,44 +12,43 @@ if (empty($options)) {
 	die;
 }
 
-$application_svn_checkout = realpath($options['c']);
-$pot_svn_checkout = realpath($options['p']);
-$makepot_project = str_replace('-', '_', $options['m']);
+$application_svn_checkout = realpath( $options['c'] );
+$pot_svn_checkout = realpath( $options['p'] );
+$makepot_project = str_replace( '-', '_', $options['m'] );
 $pot_name = $options['n'];
-$no_branch_dirs = isset($options['s']);
+$no_branch_dirs = isset( $options['s'] );
 
-$makepot = new MakePOT(false);
+$makepot = new MakePOT;
 
 $versions = array();
 
-chdir($application_svn_checkout);
-system("svn up");
-if (is_dir("trunk")) $versions[] = 'trunk';
-$branches = glob('branches/*');
-if (false !== $branches) $versions = array_merge($versions, $branches);
-$tags = glob('tags/*');
-if (false !== $tags) $versions = array_merge($versions, $tags);
+chdir( $application_svn_checkout );
+system( 'svn up' );
+if ( is_dir( 'trunk' ) ) $versions[] = 'trunk';
+$branches = glob( 'branches/*' );
+if ( false !== $branches ) $versions = array_merge( $versions, $branches );
+$tags = glob( 'tags/*' );
+if ( false !== $tags ) $versions = array_merge( $versions, $tags );
 
-if ($no_branch_dirs)
-	$versions = array('.');
-
-chdir($pot_svn_checkout);
-system("svn up");
-foreach($versions as $version) {
-	$pot = "$version/$pot_name";
-	$exists = is_file($pot);
-	// do not update old tag pots
-	if ('tags/' == substr($version, 0, 5) && $exists) continue;
-	if (!is_dir($version)) system("svn mkdir $version");
-	$real_application_svn_checkout = realpath($application_svn_checkout);
-	call_user_func(array(&$makepot, $makepot_project), "$real_application_svn_checkout/$version", "$pot_svn_checkout/$pot");
-	if (!$exists) system("svn add $pot");
-	// do not commit if the difference is only in the header, but always commit a new file
-	if (!$exists || `svn diff $pot | wc -l` > 13) {
-		preg_match('/Revision:\s+(\d+)/', `svn info $real_application_svn_checkout/$version`, $matches);
-		$logmsg = isset($matches[1]) && intval($matches[1])? "POT, generated from r".intval($matches[1]) : 'Automatic POT update';
-		system("svn ci $pot --message='$logmsg'");
-	}
+if ( $no_branch_dirs ) {
+	$versions = array( '.' );
 }
 
-?>
+chdir( $pot_svn_checkout );
+system( 'svn up' );
+foreach( $versions as $version ) {
+	$pot = "$version/$pot_name";
+	$exists = is_file( $pot );
+	// do not update old tag pots
+	if ( 'tags/' == substr( $version, 0, 5 ) && $exists ) continue;
+	if ( !is_dir( $version ) ) system( "svn mkdir $version" );
+	$real_application_svn_checkout = realpath( $application_svn_checkout );
+	call_user_func( array( &$makepot, $makepot_project ), "$real_application_svn_checkout/$version", "$pot_svn_checkout/$pot" );
+	if ( !$exists ) system( "svn add $pot" );
+	// do not commit if the difference is only in the header, but always commit a new file
+	if ( !$exists || `svn diff $pot | wc -l` > 13 ) {
+		preg_match( '/Revision:\s+(\d+)/', `svn info $real_application_svn_checkout/$version`, $matches );
+		$logmsg = isset( $matches[1] ) && intval( $matches[1] )? "POT, generated from r".intval($matches[1]) : 'Automatic POT update';
+		system( "svn ci $pot --message='$logmsg'" );
+	}
+}
