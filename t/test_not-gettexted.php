cfda555d2aceb0cf8bdfb25217d9f0412d28b524
Nikolay Bachiyski <nb@nikolay.bg>
2010-03-05 14:33:23 +0000

    Update test for NotGettexted
    git-svn-id: https://i18n.svn.wordpress.org/tools/trunk@11944 2066f16a-a1ef-0310-b9f2-d44eba5528fd

--- t/test_not-gettexted.php
+++ t/test_not-gettexted.php
@@ -6,17 +6,22 @@
  * @package wordpress-i18n
  * @subpackage tools
  */
-error_reporting(E_ALL);
-require_once('PHPUnit/Framework.php');
-require_once('../not-gettexted.php');
+error_reporting( E_ALL );
+require_once( 'PHPUnit/Framework.php' );
+require_once( '../not-gettexted.php' );
 
 class Test_Not_Gettexted extends PHPUnit_Framework_TestCase {
+	
+	function __construct() {
+		$this->ng = new NotGettexted;
+	}
+	
 	function test_make_string_aggregator() {
 		global $baba;
-		$f = make_string_aggregator('baba');
-		call_user_func($f, 'x', 'y', 'z');
-		call_user_func($f, 'a', 'b', 'c');
-		$this->assertEquals(array(array('x', 'y', 'z'), array('a', 'b', 'c')), $baba);
+		$f = $this->ng->make_string_aggregator( 'baba', 'baba.php' );
+		call_user_func( $f, 'x', 'y', 'z' );
+		call_user_func( $f, 'a', 'b', 'c' );
+		$this->assertEquals( array( array( 'x', 'y', 'baba.php', 'z'), array( 'a', 'b', 'baba.php', 'c' ) ), $baba );
 	}
 
 	function test_walk() {
@@ -27,19 +32,17 @@ echo /* WP_I18N_GUGU*/ 	"yes" /* /WP_I18N_UGU		*/;
 	if ($x == "18181") { wp_die(sprintf(/*WP_I18N_DIE*/\'We died %d times!\'/*WP_I18N_DIE*/)); }
 ?>';
 		$tokens = token_get_all($code);	
-		$this->assertEquals('', walk_tokens($tokens, 'ignore_token', 'ignore_token'));
-		$this->assertEquals('"yes"\'We died %d times!\'', walk_tokens($tokens, 'unchanged_token', 'ignore_token'));
-		$this->assertEquals($code, walk_tokens($tokens, 'unchanged_token', 'unchanged_token'));
-		$this->assertEquals($code, walk_tokens($tokens, 'unchanged_token', 'unchanged_token'));
+		$this->assertEquals( '', $this->ng->walk_tokens( $tokens, array(&$this->ng, 'ignore_token'), array(&$this->ng, 'ignore_token') ) );
+		$this->assertEquals( '"yes"\'We died %d times!\'', $this->ng->walk_tokens( $tokens, array(&$this->ng, 'unchanged_token'), array(&$this->ng, 'ignore_token') ) );
+		$this->assertEquals( $code, $this->ng->walk_tokens( $tokens, array(&$this->ng, 'unchanged_token'), array(&$this->ng, 'unchanged_token') ) );
+		$this->assertEquals( $code, $this->ng->walk_tokens( $tokens, array(&$this->ng, 'unchanged_token'), array(&$this->ng, 'unchanged_token') ) );
 	}
 
 	function test_replace() {
 		# copy to a new file, so that we don't corrupt the old one
-		copy('data/not-gettexted-0.php', 'data/not-gettexted-0-work.php');
-		command_replace('data/not-gettexted-0.mo', 'data/not-gettexted-0-work.php');
-		$this->assertEquals(file_get_contents('data/not-gettexted-0-result.php'), file_get_contents('data/not-gettexted-0-work.php'));
-		unlink();
+		copy( 'data/not-gettexted-0.php', 'data/not-gettexted-0-work.php' );
+		$this->ng->command_replace( 'data/not-gettexted-0.mo', 'data/not-gettexted-0-work.php' );
+		$this->assertEquals( file_get_contents( 'data/not-gettexted-0-result.php' ), file_get_contents( 'data/not-gettexted-0-work.php' ) );
+		unlink( 'data/not-gettexted-0-work.php' );
 	}
-
 }
-?>
