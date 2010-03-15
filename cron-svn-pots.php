0ec4e91ef01553f84fa595d6b5e995a56f5e52f6
Nikolay Bachiyski <nb@nikolay.bg>
2010-03-15 10:44:52 +0000

    Allow application and POT directory to be sub-directories of the main checkouts
    git-svn-id: https://i18n.svn.wordpress.org/tools/trunk@12017 2066f16a-a1ef-0310-b9f2-d44eba5528fd

--- cron-svn-pots.php
+++ cron-svn-pots.php
@@ -1,13 +1,15 @@
 <?php
 require_once 'makepot.php';
 
-$options = getopt( 'c:p:m:n:s' );
+$options = getopt( 'c:p:m:n:sa:b:' );
 if ( empty( $options) ) {
 ?>
 	-c	Application svn checkout
 	-p	POT svn checkout
 	-m	MakePOT project
 	-n	POT filename
+	-a	Relative path of application inside version dir in -c
+	-b	Relative patch of POT dir inside version dir in -p
 <?php
 	die;
 }
@@ -17,7 +19,8 @@ $pot_svn_checkout = realpath( $options['p'] );
 $makepot_project = str_replace( '-', '_', $options['m'] );
 $pot_name = $options['n'];
 $no_branch_dirs = isset( $options['s'] );
-
+$relative_application_path = isset( $options['a'] )? '/'.$options['a'] : '';
+$relative_pot_path = isset( $options['b'] )? '/'.$options['b'] : '';
 $makepot = new MakePOT;
 
 $versions = array();
@@ -35,20 +38,24 @@ if ( $no_branch_dirs ) {
 }
 
 chdir( $pot_svn_checkout );
-system( 'svn up' );
+if ( $application_svn_checkout != $pot_svn_checkout) system( 'svn up' );
+$real_application_svn_checkout = realpath( $application_svn_checkout );
 foreach( $versions as $version ) {
-	$pot = "$version/$pot_name";
+	$application_path = "$real_application_svn_checkout/$version{$relative_application_path}";
+	if ( !is_dir( $application_path ) ) continue;
+	$pot = "$version{$relative_pot_path}/$pot_name";
 	$exists = is_file( $pot );
 	// do not update old tag pots
 	if ( 'tags/' == substr( $version, 0, 5 ) && $exists ) continue;
 	if ( !is_dir( $version ) ) system( "svn mkdir $version" );
-	$real_application_svn_checkout = realpath( $application_svn_checkout );
-	call_user_func( array( &$makepot, $makepot_project ), "$real_application_svn_checkout/$version", "$pot_svn_checkout/$pot" );
+	if ( !is_dir(dirname("$pot_svn_checkout/$pot")) ) continue;
+	call_user_func( array( &$makepot, $makepot_project ), $application_path, "$pot_svn_checkout/$pot" );
 	if ( !$exists ) system( "svn add $pot" );
 	// do not commit if the difference is only in the header, but always commit a new file
 	if ( !$exists || `svn diff $pot | wc -l` > 13 ) {
-		preg_match( '/Revision:\s+(\d+)/', `svn info $real_application_svn_checkout/$version`, $matches );
-		$logmsg = isset( $matches[1] ) && intval( $matches[1] )? "POT, generated from r".intval($matches[1]) : 'Automatic POT update';
-		system( "svn ci $pot --message='$logmsg'" );
+		preg_match( '/Revision:\s+(\d+)/', `svn info $application_path`, $matches );
+		$logmsg = isset( $matches[1] ) && intval( $matches[1] )? "POT, generated from r".intval( $matches[1] ) : 'Automatic POT update';
+		$target = $exists? $pot : $version;
+		system( "svn ci $target --message='$logmsg'" );
 	}
 }
